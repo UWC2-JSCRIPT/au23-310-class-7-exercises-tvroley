70fa6d838f7c28577b5fd1d4bd6a506d51d860fc
Theodore Roley <tvroley@uw.edu>
2023-11-26 17:27:01 -0800

    valid length method

--- form-validation-custom.js
+++ form-validation-custom.js
@@ -1,1 +1,12 @@
-// TODO
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const form = document.getElementById("connect-form");
+const email = document.getElementById("email");
+
+const validLength = (input, min) => {
+    if(input.value.trim().length > min) {
+        input.parentElement.classList.remove("invalid");
+    } else {
+        input.parentElement.classList.add("invalid");
+    }
+};
