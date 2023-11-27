89a7400feaeca2888fb486bc5973c074631fc993
Theodore Roley <tvroley@uw.edu>
2023-11-27 00:01:03 -0800

    Added job html elements and hiding logic and listener

--- contact-me.js
+++ contact-me.js
@@ -1,10 +1,21 @@
 const selectReasonEl = document.getElementById('contact-reason');
+const codeLanguageEl = document.getElementById('code-language');
+const websiteEl = document.getElementById('company-website');
+const jobTitleEl = document.getElementById('job-title');
 
 const handleselect = (selectElement) => {
     const selectedValue = selectElement.value;
-    if(selectedValue === 'business') {
-
-    } else if(selectedValue === 'technical') {
-
+    if(selectedValue == 'Job opportunity') {
+        codeLanguageEl.parentElement.classList.add('hidden');
+        websiteEl.parentElement.classList.remove('hidden');
+        jobTitleEl.parentElement.classList.remove('hidden');
+    } else if(selectedValue == 'Talk code') {
+        codeLanguageEl.parentElement.classList.remove('hidden');
+        websiteEl.parentElement.classList.add('hidden');
+        jobTitleEl.parentElement.classList.add('hidden');
     }
 };
+
+selectReasonEl.addEventListener('change', (e) => {
+    handleselect(e.target);
+});
