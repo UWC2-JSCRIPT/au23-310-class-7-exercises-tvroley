3db219c749e7bceed5a875227ff501c1fc48d3f4
Theodore Roley <tvroley@uw.edu>
2023-11-26 22:33:57 -0800

    handleselect function, made elements required

--- contact-me.js
+++ contact-me.js
@@ -1,1 +1,9 @@
 // TODO
+const handleselect = (selectElement) => {
+    const selectedValue = selectElement.value;
+    if(selectedValue === 'business') {
+
+    } else if(selectedValue === 'technical') {
+
+    }
+};
