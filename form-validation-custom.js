fd513fa6346ff366d35c2c1dc3331110deb158e9
Theodore Roley <tvroley@uw.edu>
2023-11-26 18:22:09 -0800

    email validation

--- form-validation-custom.js
+++ form-validation-custom.js
@@ -1,7 +1,7 @@
 const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
 const form = document.getElementById("connect-form");
-const email = document.getElementById("email");
+const emailField = document.getElementById("email");
 
 const validLength = (input, min) => {
     if(input.value.trim().length > min) {
@@ -11,8 +11,19 @@ const validLength = (input, min) => {
     }
 };
 
+const validateEmail = (emailField) => {
+    const re = /\w+@\w+\.\w+/;
+    if(re.test(email.value.trim())) {
+        emailField.parentElement.classList.remove("invalid");
+    } else {
+        emailField.parentElement.classList.add("invalid");
+    }
+};
+
 form.addEventListener("submit", (e) => {
+    //default behavior not stopping without this before the validation
     e.preventDefault();
-    validLength(firstName, 3);
-    validLength(lastName, 3);
+    if (!!(validLength(firstName, 3) + validLength(lastName, 3) + validateEmail(emailField))) {
+        e.preventDefault();
+    }
 });
