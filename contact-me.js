8c6744cfeaabf6480e2b8bafa2335eaa6b7bd600
Theodore Roley <tvroley@uw.edu>
2023-11-27 19:07:04 -0800

    email and field length validation

--- contact-me.js
+++ contact-me.js
@@ -2,6 +2,10 @@ const selectReasonEl = document.getElementById('contact-reason');
 const codeLanguageEl = document.getElementById('code-language');
 const websiteEl = document.getElementById('company-website');
 const jobTitleEl = document.getElementById('job-title');
+const contactNameEl = document.getElementById("contact-name");
+const emailFieldEl = document.getElementById("contact-email");
+const messageEl = document.getElementById("message");
+const contactForm = document.getElementById("contact-me-form");
 
 const handleselect = (selectElement) => {
     const selectedValue = selectElement.value;
@@ -15,6 +19,41 @@ const handleselect = (selectElement) => {
         jobTitleEl.parentElement.classList.add('hidden');
     }
 };
+
+const validLength = (input, min) => {
+    if(input.value.trim().length >= min) {
+        input.parentElement.classList.remove("invalid");
+        
+        return true;
+    } else {
+        input.parentElement.classList.add("invalid");
+        if(input.id === 'contact-name') {
+            alert('name not long enough');
+        } else if(input.id === 'message') {
+            alert('message not long enough')
+        }
+        
+        return false;
+    }
+};
+
+const validateEmail = (emailField) => {
+    const re = /\w+@\w+\.\w+/;
+    if(re.test(email.value.trim())) {
+        emailField.parentElement.classList.remove("invalid");
+        return true;
+    } else {
+        emailField.parentElement.classList.add("invalid");
+        alert('invalid email format');
+        return false;
+    }
+};
+
+contactForm.addEventListener("submit", (e) => {
+    if (!(validLength(contactNameEl, 3) && validLength(messageEl, 10) && validateEmail(emailFieldEl))) {
+        e.preventDefault();
+    }
+});
 
 selectReasonEl.addEventListener('change', (e) => {
     handleselect(e.target);
