0f962aca1533e6b7e3490c8cfadec62ab52ab474
Theodore Roley <tvroley@uw.edu>
2023-11-27 22:40:01 -0800

    blackjack unit test for calculating score of a hand

---
+++ blackjack.spec.js
@@ -0,0 +1,13 @@
+describe('Tests for the blackjack game', () => {
+    it('should calculate the score of a hand', () => {
+        const hand = [
+            {displayVal: 'Six', val: 6, suit: 'hearts'},
+            {displayVal: 'Seven', val: 7, suit: 'hearts'}
+        ];
+
+        const result = calcPoints(hand);
+
+        expect(result.total).toEqual(13);
+        expect(result.isSoft).toEqual(false);        
+    });
+});
